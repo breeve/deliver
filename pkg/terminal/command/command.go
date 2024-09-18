ee590613f3a494fb15555a7c77f80251738312cc
breeve <1205034601@qq.com>
2024-09-18 10:33:51 +0000

    a

---
+++ pkg/terminal/command/command.go
@@ -0,0 +1,17 @@
+package command
+
+import (
+	"github.com/spf13/cobra"
+)
+
+type Command struct {
+	cmd *cobra.Command
+}
+
+func (c *Command) Execute(argOrigin string) (stdOut string, stdErr string, err error) {
+	return "", "", nil
+}
+
+func (c *Command) CommandPath() string {
+	return c.cmd.CommandPath()
+}
